754de40a3b0e8e03d8814e8ccefbe0b867a33bd7
DocThomus <bonninthom2@gmail.com>
2025-01-26 16:42:30 +0100

    Application de bruit en couches

--- procedural-planets/ShapeGenerator.cs
+++ procedural-planets/ShapeGenerator.cs
@@ -5,22 +5,35 @@ using System;
 [Tool]
 public partial class ShapeGenerator
 {
-    private ShapeSetting ShapeSetting { get; set; }
+    private ShapeSetting settings { get; set; }
 
     public ShapeGenerator(ShapeSetting shapeSetting)
     {
-        ShapeSetting = shapeSetting;
+        settings = shapeSetting;
     }
 
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnSphere)
     {
         float elevation = Evaluate(pointOnSphere);
-        return pointOnSphere * ShapeSetting.planetRadius * (1+elevation);
+        return pointOnSphere * settings.planetRadius * (1+elevation);
     }
 
     public float Evaluate(Vector3 point)
     {
-        return (ShapeSetting.Noise.GetNoise3Dv(point) + 1) * .5f;
+        float noiseValue = 0;
+        float frequency = settings.baseRoughness;
+        float amplitude = 1;
+
+        for (int i = 0; i < settings.numLayers; i++)
+        {
+            float v = settings.Noise.GetNoise3Dv(point * frequency + settings.offset);
+            noiseValue += (v + 1) * .5f * amplitude;
+            frequency *= settings.roughness;
+            amplitude *= settings.persistence;
+        }
+
+        noiseValue = Mathf.Max(0, noiseValue - settings.minValue);
+        return noiseValue * settings.strength;
     }
 
 }
--- procedural-planets/ShapeSetting.cs
+++ procedural-planets/ShapeSetting.cs
@@ -14,6 +14,90 @@ public partial class ShapeSetting : Resource
             _planetRadius = value;
             EmitChanged();
         }
+    }    
+
+    private float _strength = 1;
+    [Export]
+    public float strength
+    {
+        get => _strength;
+        set
+        {
+            _strength = value;
+            EmitChanged();
+        }
+    }
+
+    private int _numLayers = 1;
+    [Export]
+    public int numLayers
+    {
+        get => _numLayers;
+        set
+        {
+            _numLayers = value;
+            EmitChanged();
+        }
+    }
+
+    private float _baseRoughness = 1;
+    [Export]
+    public float baseRoughness
+    {
+        get => _baseRoughness;
+        set
+        {
+            _baseRoughness = value;
+            EmitChanged();
+        }
+    }
+
+    private float _roughness = 2;
+    [Export]
+    public float roughness
+    {
+        get => _roughness;
+        set
+        {
+            _roughness = value;
+            EmitChanged();
+        }
+    }
+
+    private float _persistence = .5f;
+    [Export]
+    public float persistence
+    {
+        get => _persistence;
+        set
+        {
+            _persistence = value;
+            EmitChanged();
+        }
+    }
+
+    private Vector3 _offset;
+    [Export]
+    public Vector3 offset
+    {
+        get => _offset;
+        set
+        {
+            _offset = value;
+            EmitChanged();
+        }
+    }
+
+    private float _minValue;
+    [Export]
+    public float minValue
+    {
+        get => _minValue;
+        set
+        {
+            _minValue = value;
+            EmitChanged();
+        }
     }
 
     private FastNoiseLite _Noise = GD.Load<FastNoiseLite>("res://PlanetSettings/Noise.tres");
@@ -27,4 +111,6 @@ public partial class ShapeSetting : Resource
             EmitChanged();
         }
     }
+
+
 }
